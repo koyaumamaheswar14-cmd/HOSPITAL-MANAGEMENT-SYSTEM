dd793e1fc1bf09505a92d1c20352eae4087ac1e4
KOYA UMA MAHESWAR <koyaumamaheswar.14@gmail.com>
2025-08-23 20:59:47 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,13 @@
+package com.hms.util;
+
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/hmsdb";
+    private static final String USER = "root";      // your MySQL username
+    private static final String PASS = "password";  // your MySQL password
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
