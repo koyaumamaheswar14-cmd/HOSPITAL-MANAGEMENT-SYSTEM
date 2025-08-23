9247b396e17292d1ae9791c6c876b6a488f2c84d
KOYA UMA MAHESWAR <koyaumamaheswar.14@gmail.com>
2025-08-23 22:04:27 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/hmsdb";
     private static final String USER = "root";      // your MySQL username
-    private static final String PASS = "password";  // your MySQL password
+    private static final String PASS = "Uma";  // your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
